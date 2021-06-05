9512600887571ffc50b2169134f6a0b644796360
hhsj <zhou90655@gmail.com>
2021-06-05 22:48:58 +0800

    2121

--- src/data/data.js
+++ src/data/data.js
@@ -16,13 +16,13 @@ export const baselist = [
   // { 'code': 'klc', 'label': 'PC蛋蛋', 'status': true, 'children': [] },
   // { 'code': 'ssl', 'label': '时时乐', 'status': true, 'children': [] },
 
-  { 'code': 'keno', 'label': '快8', 'status': true, 'children': [] },
+  { 'code': 'keno', 'label': '快乐彩', 'status': true, 'children': [] },
   { 'code': 'lhc', 'label': '六合彩', 'status': true, 'children': [] },
   { 'code': 'x5', 'label': '11选5', 'status': true, 'children': [] },
   { 'code': 'ssc', 'label': '时时彩', 'status': true, 'children': [] },
   { 'code': 'k3', 'label': '快3', 'status': true, 'children': [] },
-  { 'code': 'pk10', 'label': 'PK10', 'status': true, 'children': [] },
-  { 'code': 'dpc', 'label': '3D', 'status': true, 'children': [] }]
+  { 'code': 'pk10', 'label': '赛车飞艇', 'status': true, 'children': [] },
+  { 'code': 'dpc', 'label': '低频彩', 'status': true, 'children': [] }]
 export const zxkd3 = [10, 54, 96, 126, 144, 150, 144, 126, 96, 54]
 export const zxhz3 = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 63, 69, 73, 75, 75, 73, 69, 63, 55, 45, 36, 28, 21, 15, 10, 6, 3, 1]
 export const zuxhz3 = [0, 1, 2, 2, 4, 5, 6, 8, 10, 11, 13, 14, 14, 15, 15, 14, 14, 13, 11, 10, 8, 6, 5, 4, 2, 2, 1]
